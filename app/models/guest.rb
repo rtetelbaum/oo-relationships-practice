cb8b49d2813bc1a14b43a8dc933887ff7d04390f
rtetelbaum <rtetelbaum@mailbox.org>
2020-09-28 23:31:10 -0400

    done all methods

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -7,6 +7,26 @@ class Guest
 		@@all << self
 	end
 
+	def trips
+		Trip.all.find_all { |trip| trip.guest == self }
+	end
+
+	def listings
+		self.trips.collect { |trip| trip.listing }
+	end
+
+	def trip_count
+		self.trips.size
+	end
+
+	def self.pro_traveller
+		self.all.find_all { |guest| guest.trip_count > 1 }
+	end
+
+	def self.find_all_by_name(guest_name)
+		self.all.find_all { |guest| guest.name == guest_name }
+	end
+
 	def self.all
 		@@all
 	end

5f247fff353a88a8572900cb353dcc56f1917024
rtetelbaum <rtetelbaum@mailbox.org>
2020-09-28 23:23:43 -0400

    done listing methods

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,9 +7,24 @@ class Listing
 		@@all << self
 	end
 
+	def trips
+		Trip.all.find_all { |trip| trip.listing == self }
+	end
+
 	def guests
-		trips = Trip.all.find_all { |trip| trip.listing == self }
-		trips.collect { |trip| trip.guest }
+		self.trips.collect { |trip| trip.guest }
+	end
+
+	def trip_count
+		self.trips.size
+	end
+
+	def self.find_all_by_city(city_name)
+		self.all.find_all { |listing| listing.city == city_name }
+	end
+
+	def self.most_popular
+		self.all.max_by { |listing| listing.trip_count }
 	end
 
 	def self.all
